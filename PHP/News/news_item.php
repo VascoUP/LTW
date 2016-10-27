36e1b8177dc0be523cfc6562f1cd0589a9049a44
tiagobalm <tiagoalmeida.95@hotmail.com>
2016-10-27 11:18:25 +0100

    Exercise 4 finished

--- PHP/News/news_item.php
+++ PHP/News/news_item.php
@@ -3,12 +3,21 @@
 	<head>
 		<title>CSS Exercise</title>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="news_ficheiros/style1.css">
+		<link rel="stylesheet" href="css/style1.css">
 		</head>
 	<body>
   	<div id="header">
 			<h1>Online Newspaper</h1>
 			<h2>CSS Exercise</h2>
+		</div>
+		<div id="menu">
+			<ul>
+				<li><a href="">Politics</a></li>
+				<li><a href="">Sports</a></li>
+				<li><a href="">World</a></li>
+				<li><a href="">Education</a></li>
+				<li><a href="">Society</a></li>
+			</ul>
 		</div>
 		<div id="content">
       <?php
