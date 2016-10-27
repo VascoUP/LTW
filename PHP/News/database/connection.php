fbed87b542506c3b49309d7cbc82de238c3777a3
VascoUP <up201403485@fe.up.pt>
2016-10-27 11:20:22 +0100

    Exercise 5 done

--- PHP/News/database/connection.php
+++ PHP/News/database/connection.php
@@ -1,3 +1,4 @@
 <?php
     $db = new PDO('sqlite:database/news.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
--- PHP/News/list_news.php
+++ PHP/News/list_news.php
@@ -22,23 +22,9 @@
 		<div id="content">
       <?php
 				include_once('database/connection.php');
+				include_once('database/news.php');
 
-        $stmt = $db->prepare('SELECT * FROM news');
-        $stmt->execute();  
-        $result = $stmt->fetchAll();
-
-        foreach( $result as $row) {
-          echo '<div class="news-item">';
-          echo '<h3>' . $row['title'] . '</h3>';
-          echo '<p class="introduction">' . $row['introduction'] . '</p>';
-          echo '<p>' . $row['fulltext'] . '</p>';
-          echo 	'<ul>
-					            <li><a href="https://web.fe.up.pt/%7Earestivo/page/files/exercises/css/noticia1.html">see more</a></li>
-					            <li><a href="https://web.fe.up.pt/%7Earestivo/page/files/exercises/css/comentarios1.html">comments (2)</a></li>
-					            <li><a href="https://web.fe.up.pt/%7Earestivo/page/files/exercises/css/partilhar1.html">share</a></li>
-				        </ul>';
-					echo '</div>';
-        }
+				$result = getAllNews();
       ?>
     </div>
 		<div id="footer">
