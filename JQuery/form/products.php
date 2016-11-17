00a66ae7ec0be475a1e03fefbcfe4fabc56031dd
VascoUP <vasco_mag_pereira@hotmail.com>
2016-11-17 11:36:29 +0000

    End of class

---
+++ JQuery/form/products.php
@@ -0,0 +1,5 @@
+<?php
+	$p = array('0' => "ABCD" ,'1' => "DEFG");
+	$data = json_encode($p);
+	echo $data;
+?>
--- JQuery/form/script.js
+++ JQuery/form/script.js
@@ -1,1 +1,36 @@
-var products = ['ABCD', 'DEFG'];
+var products = [];
+
+$(document).ready( function() { loadDocument() } )
+
+function loadDocument () {
+	var products_select = $('#products').children().children('select');
+
+	$.getJSON('products.php', 
+		function( data ) {
+			products = data;
+
+			loadProducts(products_select);	
+		});
+
+	window.alert($('#products input'));
+
+	$('#products').children('input').last().click(
+		function() {
+			addLine();
+		});
+}
+
+function loadProducts ( select ) {
+	for( var i = 0; i < products.length; i++ ) {
+		select.append($('<option></option>', {
+			value : products[i],
+			text : products[i]
+		}));
+	}
+}
+
+function addLine () {
+	var line = $('#products').children().first().html();
+	$('#products').children('input').before( $('<div class=\"line\">' + line + '</div>') );
+}
+
