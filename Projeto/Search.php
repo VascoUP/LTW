5d28ad3a1ffe6a14e98e8db8dc66850a9e0502d2
VascoUP <vasco_mag_pereira@hotmail.com>
2016-11-24 11:36:58 +0000

    Started working on the project

---
+++ Projeto/Search.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<tite>AELIUS</tite>
+	</head>
+	<body>
+		<div id="header_page">
+			<img src="Images/Logo.png" alt="AELIUS" width="100" height="100" />
+			<ul id="login">
+				<li>Register</li>
+				<li>Login</li>
+				<li>Logout</li>
+			<ul>
+		</div>
+	</body>
+</html>
