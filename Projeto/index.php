f0c260c0911edc84a167cd9d3157670145360c6b
VascoUP <vasco_mag_pereira@hotmail.com>
2016-11-29 22:39:04 +0000

    Restaurant photo with basic info

--- Projeto/index.php
+++ Projeto/index.php
@@ -1,3 +1,3 @@
 <?php
-  header ('Location: search.php');
+  header ('Location: restaurant.php');
 ?>
---
+++ Projeto/templates/restaurant_photo.php
@@ -0,0 +1,11 @@
+<div class="Margin_Top_Bottom">
+<div class="Restaurant_Photo">
+	<!-- <img src="images/background_search.jpg" alt="restaurant" /> -->
+	<div class="Restaurant_Basic_Info">	
+		<h3 class="Restaurant_Name"> Restaurant </h3>
+		<?php
+			include ('templates/score.php');
+		?>
+	</div>
+</div>
+</div>
